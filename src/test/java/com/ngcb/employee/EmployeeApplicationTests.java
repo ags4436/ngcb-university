15cd6e25dedc13089bb1d244fba8affb1cc49273
ags4436 <kvnaakash01@gmail.com>
2022-10-06 08:53:22 +0530

    MainApp refactor

--- src/test/java/com/ngcb/employee/EmployeeApplicationTests.java
+++ src/test/java/com/ngcb/employee/EmployeeApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Homework1ApplicationTests {
+class EmployeeApplicationTests {
 
 	@Test
 	void contextLoads() {
