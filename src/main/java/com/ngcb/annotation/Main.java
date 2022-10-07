4d6db6de123a1aa1e6f449ea60520febf0865cf4
ags4436 <kvnaakash01@gmail.com>
2022-10-07 15:01:59 +0530

    Method Annotation

--- src/main/java/com/ngcb/annotation/Main.java
+++ src/main/java/com/ngcb/annotation/Main.java
@@ -3,4 +3,8 @@ package com.ngcb.annotation;
 @SuppressWarnings("deprecation")
 @MyAnnotation(message = "Calling Annotation from Class")
 public class Main {
+  @MethodAnnotation
+  public void TestingMethodAnnotation() {
+    System.out.println("Hello");
+  }
 }
--- src/main/java/com/ngcb/annotation/MethodAnnotation.java
+++ src/main/java/com/ngcb/annotation/MethodAnnotation.java
@@ -5,10 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Deprecated
-@Target({ElementType.METHOD,ElementType.TYPE})
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.SOURCE)
-public @interface MyAnnotation {
-    String message() default "No Message";
+public @interface MethodAnnotation {
+    String author() default "AGS";
 
 }
